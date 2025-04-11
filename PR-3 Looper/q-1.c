b5a34487b20076dfd4f4370c4ef17ffe3efaf2ef
shailesh-2016 <snmodi2016@gmail.com>
2025-04-11 12:08:39 +0530

    all

---
+++ PR-3 Looper/q-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  
+    int i=0;
+    while (i<27)
+    {
+        printf(" %c", 'a' + i);
+        i+=4;
+    }
+    return 0;
+}
---
+++ PR-4 Circlet/q-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num=11;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j=1; j <= i; j++)
+        {
+            printf(" %d", num+1);
+        }
+        printf("\n");
+    }
+}
