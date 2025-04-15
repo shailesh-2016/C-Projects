dbdc9115097ac92a698ed34cf9e9ed74eeabafbc
shailesh-2016 <snmodi2016@gmail.com>
2025-04-15 11:49:34 +0530

    all done

--- PR-4 Circlet/q-2.c
+++ PR-4 Circlet/q-2.c
@@ -2,11 +2,12 @@
 int main()
 {
     int num=11;
-    for (int i = 1; i <= 5; i++)
+    for (int i = 1; i <= 4; i++)
     {
         for (int j=1; j <= i; j++)
         {
-            printf(" %d", num+1);
+            printf(" %d", num);
+            num++;
         }
         printf("\n");
     }
